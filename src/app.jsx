af645ff3f5d677970129f49a1f0bf7c2e3b25bd4
Sergey Rubets <srubets@speroteck.com>
2017-03-29 14:53:58 +0300

    Header view was added

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/index.scss';
 
+import Header from './Components/Header';
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className="redBg">module</span> local styles.</p>
-        <p>Enjoy!</p>
+        <Header />
       </div>
     )
   }
